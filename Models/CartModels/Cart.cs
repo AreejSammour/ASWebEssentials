8e2c51ad19c0dd14b7d727a2e41934a253291a36
Areej Sammour <122778464+AreejSammour@users.noreply.github.com>
2024-06-19 17:37:19 +0200

    Add files via upload

---
+++ Models/CartModels/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASWebEssentials.Models.CartModels
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public List<CartItem>? Items { get; set;}
+
+        [Range(0.01, double.MaxValue)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TotalPrice { get; set; }
+        public int TotalQuantity { get; set; }
+        public string? CartStatus { get; set; }
+        public AppUser? User { get; set; }
+    }
+}

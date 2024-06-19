5125f44669af9bd55543ee8018c9c83fed20270a
Areej Sammour <122778464+AreejSammour@users.noreply.github.com>
2024-06-19 17:39:29 +0200

    Add files via upload

---
+++ Models/CartModels/CartItem.cs
@@ -0,0 +1,33 @@
+﻿using ASWebEssentials.Models.ProductModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASWebEssentials.Models.CartModels
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+        [Required]
+
+        [ForeignKey("Product")]
+        public int? ProdId { get; set; }
+        public Product? Product { get; set; }
+        [Required]
+
+        [ForeignKey("Cart")]
+
+        public int? CartId {  get; set; }
+        public Cart? Cart { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal QuantPrice { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}

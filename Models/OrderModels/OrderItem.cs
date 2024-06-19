8382d6faf630b0061fa9a64959a8d2869969ae85
Areej Sammour <122778464+AreejSammour@users.noreply.github.com>
2024-06-19 17:41:54 +0200

    Add files via upload

---
+++ Models/OrderModels/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using ASWebEssentials.Models.CartModels;
+
+namespace ASWebEssentials.Models.OrderModels
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public Cart? Cart { get; set; }
+        public string? OrderItemId { get; set; }
+        public int OrdersId { get; set; }
+        public Orders? Orders { get; set; }
+        public DateTime PlacedDate { get; set; } = DateTime.Now;
+        public string? Status { get; set; }
+    }
+}
---
+++ Models/OrderModels/Orders.cs
@@ -0,0 +1,9 @@
+﻿namespace ASWebEssentials.Models.OrderModels
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public AppUser? User { get; set; }
+        public List<OrderItem>? OrderItem { get; set; } 
+    }
+}
